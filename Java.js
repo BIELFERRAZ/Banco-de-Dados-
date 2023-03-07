88351e004593fe4c0a4295dd0ba671c2f38cc9d7
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-06 22:34:47 -0300

    Finalizando tela de login

--- Java.js
+++ Java.js
@@ -1,83 +1,28 @@
 const estados = [
     {
-    uf: "SP", cidades: [
-        {id: 1, nome: "Mongaguá"}
-       ,{id: 2, nome: "Itanhaém"}
-       ,{id: 3, nome: "Santos"}
+    uf: "Informática", modulo: [
+        {id: 1, nome: "1I3"}
+       ,{id: 2, nome: "2I3"}
+       ,{id: 3, nome: "3I3"}
        ] 
    }
    ,
    {
-       uf: "RJ", cidades: [
-         {id: 4, nome: "Níteroi"}
-       , {id: 5, nome: "Macaé"}
-       , {id: 6, nome: "Santa Maria Madalena"}
+       uf: "Administração", modulo: [
+        {id: 1, nome: "1I3"}
+        ,{id: 2, nome: "2I3"}
+        ,{id: 3, nome: "3I3"}
        ] 
    }
    ,
    {
-       uf: "AC", cidades: [
-        {id: 7, nome: "Feijó"}
-      , {id: 8, nome: "Xapuri"}
-      , {id: 9, nome: "Bujari"}
+       uf: "Turismo", cidades: [
+        {id: 1, nome: "1I3"}
+       ,{id: 2, nome: "2I3"}
+       ,{id: 3, nome: "3I3"}
        ] 
    }
-   ,
-   {
-       uf: "MS", cidades: [
-        {id: 10, nome: "Maracaju"}
-      , {id: 11, nome: "Dourados"}
-      , {id: 12, nome: "Jardim"}
-       ] 
-   }
-   ,
-   {
-       uf: "SC", cidades: [
-        {id: 13, nome: "Bombinhas"}
-      , {id: 14, nome: "Penha"}
-      , {id: 15, nome: "Balneario Camburiú"}
-       ] 
-   }
-   ,
-   {
-       uf: "AM", cidades: [
-        {id: 16, nome: "Manaus"}
-      , {id: 17, nome: "Tefé"}
-      , {id: 18, nome: "Coari"}
-       ] 
-   }
-   ,
-   {
-       uf: "MG", cidades: [
-        {id: 19, nome: "Trindade"}
-      , {id: 20, nome: "Passa Quatro"}
-      , {id: 21, nome: "Monte Verde"}
-       ] 
-   }
-   ,
-   {
-       uf: "ES", cidades: [
-        {id: 22, nome: "Vitória"}
-      , {id: 23, nome: "Serra"}
-      , {id: 24, nome: "Colatina"}
-       ] 
-   }
-   ,
-   {
-       uf: "AL", cidades: [
-        {id: 25, nome: "Penedo"}
-      , {id: 26, nome: "Murici"}
-      , {id: 27, nome: "Maceio"}
-       ] 
-   }
-   ,
-   {
-       uf: "RR", cidades: [
-        {id: 28, nome: "Iracema"}
-      , {id: 29, nome: "Bonfim"}
-      , {id: 30, nome: "Cantá"}
-       ] 
-   }
+  
    ];
    
 
@@ -85,47 +30,47 @@ const alteraEstado = () =>{
     const selectEstado = document.getElementById("estado");
     const estadoSelecionado = selectEstado.value;
 
-    const selectCidade = document.getElementById("cidade");
+    const selectmodulo = document.getElementById("modulo");
     selectCidade.innerHTML = "";
 
     const divSaida = document.getElementById("saida");
     divSaida.innerHTML = "";
 
     if(estadoSelecionado){
-        adicionaOpcaoSelect(selectCidade, "", "Selecione uma cidade");
+        adicionaOpcaoSelect(selectmodulo, "","Selecione o modulo");
 
         const ufFiltro = estados.filter(
             estado => estado.uf === estadoSelecionado)[0];  
         
-        ufFiltro.cidades.forEach(
-            cidade => adicionaOpcaoSelect(selectCidade, cidade.id, cidade.nome)
+        ufFiltro.modulo.forEach(
+            modulo => adicionaOpcaoSelect(selectCidade, modulo.id, modulo.nome)
         );
     }
 
-    ajustaComboCidades();
+    ajustaCombomodulo();
 
 };
 
-const ajustaComboCidades = () => {
-    const selectCidade = document.getElementById("cidade");
+const ajustaCombomodulo = () => {
+    const selectmodulo = document.getElementById("modulo");
 
-    if (selectCidade.length > 0){
-        selectCidade.disabled = false;
+    if (selectmodulo.length > 0){
+        selectmodulo.disabled = false;
     }else{
-        adicionaOpcaoSelect(selectCidade, "", "---Selecione um estado---");
-        selectCidade.disabled = true;
+        adicionaOpcaoSelect(selectmodulo, "", "Selecione o modulo");
+        selectmodulo.disabled = true;
     }
 };
 
-const alteraCidade = () => {
+const alteramodulo = () => {
     const divSaida = document.getElementById("saida");
     const selectEstado = document.getElementById("estado");
-    const selectCidade = document.getElementById("cidade");
+    const selectmodulo = document.getElementById("modulo");
 
     const estadoSelecionado = selectEstado.value;
-    const cidadeSelecionada = selectCidade.options[selectCidade.selectedIndex].text;
-    if (selectCidade.value){
-        divSaida.innerHTML = `${cidadeSelecionada}/${estadoSelecionado}`;
+    const moduloSelecionada = selectmodulo.options[selectmodulo.selectedIndex].text;
+    if (selectmodulo.value){
+        divSaida.innerHTML = `${moduloSelecionada}/${estadoSelecionado}`;
     }else{
         divSaida.innerHTML = "";
     }
@@ -138,7 +83,7 @@ const adicionaOpcaoSelect = (select, value, text) => {
 
 const iniciar = () => {
     document.getElementById("estado").addEventListener("change", alteraEstado);
-    document.getElementById("cidade").addEventListener("change", alteraCidade);
+    document.getElementById("modulo").addEventListener("change", alteramodulo);
     ajustaComboCidades();
 };
 
